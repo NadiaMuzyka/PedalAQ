4ff9d584d38d0ff7e768f8fb2d7b7087396c92af
Andrea <andrealuca.disimone@gmail.com>
2025-01-17 19:46:00 +0100

    Aggiunta di getter e setter

--- src/main/java/org/pedalaq/Model/Prenotazione.java
+++ src/main/java/org/pedalaq/Model/Prenotazione.java
@@ -20,4 +20,36 @@ public class Prenotazione {
         //TODO controllare che la prenotazione sia ancora valida
         return true;
     }
+
+    public LocalDateTime getScadenza() {
+        return scadenza;
+    }
+
+    public void setScadenza(LocalDateTime scadenza) {
+        this.scadenza = scadenza;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Veicolo getVeicolo() {
+        return veicolo;
+    }
+
+    public void setVeicolo(Veicolo veicolo) {
+        this.veicolo = veicolo;
+    }
+
+    public Cittadino getCittadino() {
+        return cittadino;
+    }
+
+    public void setCittadino(Cittadino cittadino) {
+        this.cittadino = cittadino;
+    }
 }
