fe223d718277732660c152a72c26a63f1b796d0d
Andrea <andrealuca.disimone@gmail.com>
2025-01-20 12:13:05 +0100

    Aggiunta della "struttura finale" del main e delle 2 classi di view

---
+++ src/main/java/org/pedalaq/View/BusinessLogic.java
@@ -0,0 +1,12 @@
+package org.pedalaq.View;
+//BusinessLogic gestisce il comportamento principale senza preoccuparsi dell'interfaccia utente.
+
+public class BusinessLogic {
+    public void performOperation1() {
+        System.out.println("Operazione 1 completata.");
+    }
+
+    public void performOperation2() {
+        System.out.println("Operazione 2 completata.");
+    }
+}
