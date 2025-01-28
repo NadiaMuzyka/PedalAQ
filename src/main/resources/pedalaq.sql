bc8776726c2ea7150225854d7bd336d30cd5a671
Andrea <andrealuca.disimone@gmail.com>
2025-01-28 21:30:07 +0100

    Aggiornamento file.sql

--- src/main/resources/pedalaq.sql
+++ src/main/resources/pedalaq.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 27, 2025 at 01:11 PM
+-- Generation Time: Jan 28, 2025 at 09:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,7 +40,7 @@ CREATE TABLE `abbonamento` (
 
 INSERT INTO `abbonamento` (`id`, `dataFine`, `dataInizio`, `id_tariffa_abbonamento`) VALUES
 (1, '2025-01-28', '2025-01-18', 1),
-(2, '2025-01-31', '2025-01-20', 1),
+(2, '2025-12-31', '2025-01-20', 1),
 (3, '2025-01-27', '2025-01-01', 1);
 
 -- --------------------------------------------------------
@@ -65,18 +65,19 @@ CREATE TABLE `citta` (
   `id` bigint(20) NOT NULL,
   `lat` double NOT NULL,
   `lon` double NOT NULL,
-  `nome` varchar(255) DEFAULT NULL
+  `nome` varchar(255) DEFAULT NULL,
+  `id_tariffa` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `citta`
 --
 
-INSERT INTO `citta` (`id`, `lat`, `lon`, `nome`) VALUES
-(1, 42.357342, 13.367412, 'L\'Aquila'),
-(2, 42.661145, 13.698667, 'Teramo'),
-(3, 42.34796, 14.16411, 'Chieti'),
-(4, 42.461535, 14.220118, 'Pescara');
+INSERT INTO `citta` (`id`, `lat`, `lon`, `nome`, `id_tariffa`) VALUES
+(1, 42.357342, 13.367412, 'L\'Aquila', 1),
+(2, 42.661145, 13.698667, 'Teramo', NULL),
+(3, 42.34796, 14.16411, 'Chieti', NULL),
+(4, 42.461535, 14.220118, 'Pescara', NULL);
 
 -- --------------------------------------------------------
 
@@ -91,18 +92,20 @@ CREATE TABLE `cittadino` (
   `lat` double NOT NULL,
   `lng` double NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
-  `saldo` float NOT NULL,
-  `id_abbonamento_attivo` bigint(20) DEFAULT NULL
+  `saldo` double NOT NULL,
+  `id_abbonamento_attivo` bigint(20) DEFAULT NULL,
+  `puntiClassifica` int(11) NOT NULL,
+  `puntiUtilizzabili` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `cittadino`
 --
 
-INSERT INTO `cittadino` (`id`, `CF`, `cognome`, `lat`, `lng`, `nome`, `saldo`, `id_abbonamento_attivo`) VALUES
-(1, 'DSMNRL01P27A345T', 'Di Simone', 42.372549, 13.357023, 'Andrea Luca', 0, 1),
-(2, 'MZYNDA02P55A345F', 'Muzyka', 42.363443, 13.3445664, 'Nadia', 0, 2),
-(3, 'NNTNDR02M12C632G', 'Iannotti', 42.36828, 13.332279, 'Andrea', 0, 3);
+INSERT INTO `cittadino` (`id`, `CF`, `cognome`, `lat`, `lng`, `nome`, `saldo`, `id_abbonamento_attivo`, `puntiClassifica`, `puntiUtilizzabili`) VALUES
+(1, 'DSMNRL01P27A345T', 'Di Simone', 42.372549, 13.357023, 'Andrea Luca', 0, 1, 0, 0),
+(2, 'MZYNDA02P55A345F', 'Muzyka', 42.363443, 13.3445664, 'Nadia', 9966.7, 2, 0, 8100),
+(3, 'NNTNDR02M12C632G', 'Iannotti', 42.36828, 13.332279, 'Andrea', 0, 3, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -116,7 +119,9 @@ CREATE TABLE `noleggio` (
   `inizioCorsa` datetime(6) DEFAULT NULL,
   `id_prenotazione` bigint(20) DEFAULT NULL,
   `id_tariffa` bigint(20) DEFAULT NULL,
-  `id_stallo_partenza` bigint(20) DEFAULT NULL
+  `id_stallo_partenza` bigint(20) DEFAULT NULL,
+  `id_stallo_arrivo` bigint(20) DEFAULT NULL,
+  `id_cittadino` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -190,8 +195,16 @@ CREATE TABLE `tariffanoleggiopromozione` (
   `costoAlMinuto` float NOT NULL,
   `dataFine` date DEFAULT NULL,
   `dataInizio` date DEFAULT NULL,
-  `id_citta` bigint(20) DEFAULT NULL
+  `id_citta` bigint(20) DEFAULT NULL,
+  `puntiRichiesti` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tariffanoleggiopromozione`
+--
+
+INSERT INTO `tariffanoleggiopromozione` (`id`, `codice`, `costoAlMinuto`, `dataFine`, `dataInizio`, `id_citta`, `puntiRichiesti`) VALUES
+(1, NULL, 0.05, '2025-12-31', '2025-01-01', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -204,8 +217,16 @@ CREATE TABLE `tariffanoleggiostandard` (
   `costoAlMinuto` float NOT NULL,
   `dataFine` date DEFAULT NULL,
   `dataInizio` date DEFAULT NULL,
-  `id_citta` bigint(20) DEFAULT NULL
+  `id_citta` bigint(20) DEFAULT NULL,
+  `id_tariffa` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tariffanoleggiostandard`
+--
+
+INSERT INTO `tariffanoleggiostandard` (`id`, `costoAlMinuto`, `dataFine`, `dataInizio`, `id_citta`, `id_tariffa`) VALUES
+(1, 0.1, '2025-12-31', '2025-01-01', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -219,20 +240,22 @@ CREATE TABLE `veicolo` (
   `stato` varchar(255) DEFAULT NULL,
   `id_stallo` bigint(20) DEFAULT NULL,
   `VEICOLO_TYPE` varchar(31) NOT NULL,
-  `batteria` int(11) DEFAULT NULL
+  `batteria` int(11) DEFAULT NULL,
+  `lat` double NOT NULL,
+  `lon` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `veicolo`
 --
 
-INSERT INTO `veicolo` (`id`, `codiceSblocco`, `stato`, `id_stallo`, `VEICOLO_TYPE`, `batteria`) VALUES
-(1, 'AA22CC', 'Libero', 1, 'BICI', NULL),
-(2, 'BB22CC', 'Libero', 1, 'BICIELETTRICA', 78),
-(3, 'CC22CC', 'Libero', 1, 'MONOPATTINO', 99),
-(4, 'DD22CC', 'Libero', 2, 'BICI', NULL),
-(5, 'EE22CC', 'Libero', 3, 'BICIELETTRICA', 90),
-(6, 'FF22CC', 'Libero', 4, 'MONOPATTINO', 42);
+INSERT INTO `veicolo` (`id`, `codiceSblocco`, `stato`, `id_stallo`, `VEICOLO_TYPE`, `batteria`, `lat`, `lon`) VALUES
+(1, 'AA22CC', 'Libero', 1, 'BICI', NULL, 0, 0),
+(2, 'BB22CC', 'Libero', 1, 'BICIELETTRICA', 78, 0, 0),
+(3, 'CC22CC', 'Libero', 1, 'MONOPATTINO', 99, 0, 0),
+(4, 'DD22CC', 'Libero', 2, 'BICI', NULL, 0, 0),
+(5, 'EE22CC', 'Libero', 3, 'BICIELETTRICA', 90, 0, 0),
+(6, 'FF22CC', 'Libero', 4, 'MONOPATTINO', 42, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -256,7 +279,8 @@ ALTER TABLE `accessorio`
 -- Indexes for table `citta`
 --
 ALTER TABLE `citta`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UKbihwb3id5t0y6yyj9948tgrr` (`id_tariffa`);
 
 --
 -- Indexes for table `cittadino`
@@ -272,7 +296,9 @@ ALTER TABLE `noleggio`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `UKoaaqf6mw28mq5f5py4d0lc5k7` (`id_prenotazione`),
   ADD KEY `FKd8nmwwpr2jpacsobjd94t23tr` (`id_tariffa`),
-  ADD KEY `FK4099tqfgn3uidoyun5smoxnfo` (`id_stallo_partenza`);
+  ADD KEY `FK4099tqfgn3uidoyun5smoxnfo` (`id_stallo_partenza`),
+  ADD KEY `FKsrqel21c1x18aia6o8s6122rn` (`id_stallo_arrivo`),
+  ADD KEY `FK815ifgh3pw03nm8t2d283yasx` (`id_cittadino`);
 
 --
 -- Indexes for table `prenotazione`
@@ -307,7 +333,8 @@ ALTER TABLE `tariffanoleggiopromozione`
 --
 ALTER TABLE `tariffanoleggiostandard`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FKb0a6ulii0sgjrrhf600kv1f9l` (`id_citta`);
+  ADD KEY `FKb0a6ulii0sgjrrhf600kv1f9l` (`id_citta`),
+  ADD KEY `FKawtdlcul78ayasisn1qgfm60b` (`id_tariffa`);
 
 --
 -- Indexes for table `veicolo`
@@ -348,13 +375,13 @@ ALTER TABLE `cittadino`
 -- AUTO_INCREMENT for table `noleggio`
 --
 ALTER TABLE `noleggio`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `prenotazione`
 --
 ALTER TABLE `prenotazione`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `stallo`
@@ -372,13 +399,13 @@ ALTER TABLE `tariffaabbonamento`
 -- AUTO_INCREMENT for table `tariffanoleggiopromozione`
 --
 ALTER TABLE `tariffanoleggiopromozione`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tariffanoleggiostandard`
 --
 ALTER TABLE `tariffanoleggiostandard`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `veicolo`
@@ -403,6 +430,12 @@ ALTER TABLE `accessorio`
   ADD CONSTRAINT `FKi9avlqyicj37di1qc9sox5tyo` FOREIGN KEY (`id_veicolo`) REFERENCES `veicolo` (`id`);
 
 --
+-- Constraints for table `citta`
+--
+ALTER TABLE `citta`
+  ADD CONSTRAINT `FKhyb48cpl0kk4dgsviswv3f7kk` FOREIGN KEY (`id_tariffa`) REFERENCES `tariffanoleggiostandard` (`id`);
+
+--
 -- Constraints for table `cittadino`
 --
 ALTER TABLE `cittadino`
@@ -414,7 +447,9 @@ ALTER TABLE `cittadino`
 ALTER TABLE `noleggio`
   ADD CONSTRAINT `FK4099tqfgn3uidoyun5smoxnfo` FOREIGN KEY (`id_stallo_partenza`) REFERENCES `stallo` (`id`),
   ADD CONSTRAINT `FK7ec5gc3dm4l3jk41bjgg35uh2` FOREIGN KEY (`id_prenotazione`) REFERENCES `prenotazione` (`id`),
-  ADD CONSTRAINT `FKl6ouong92tejq8kx5sha0s1ka` FOREIGN KEY (`id_tariffa`) REFERENCES `tariffanoleggiopromozione` (`id`);
+  ADD CONSTRAINT `FK815ifgh3pw03nm8t2d283yasx` FOREIGN KEY (`id_cittadino`) REFERENCES `cittadino` (`id`),
+  ADD CONSTRAINT `FKl6ouong92tejq8kx5sha0s1ka` FOREIGN KEY (`id_tariffa`) REFERENCES `tariffanoleggiopromozione` (`id`),
+  ADD CONSTRAINT `FKsrqel21c1x18aia6o8s6122rn` FOREIGN KEY (`id_stallo_arrivo`) REFERENCES `stallo` (`id`);
 
 --
 -- Constraints for table `prenotazione`
@@ -439,6 +474,7 @@ ALTER TABLE `tariffanoleggiopromozione`
 -- Constraints for table `tariffanoleggiostandard`
 --
 ALTER TABLE `tariffanoleggiostandard`
+  ADD CONSTRAINT `FKawtdlcul78ayasisn1qgfm60b` FOREIGN KEY (`id_tariffa`) REFERENCES `citta` (`id`),
   ADD CONSTRAINT `FKb0a6ulii0sgjrrhf600kv1f9l` FOREIGN KEY (`id_citta`) REFERENCES `citta` (`id`);
 
 --
